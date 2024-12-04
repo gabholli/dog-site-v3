0e3deb3c1a45eb5d097bd6a8b447ff10b56067ef
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-12-04 11:50:39 -0800

    Updated colors

--- dog-site-v3/src/app/components/Header.tsx
+++ dog-site-v3/src/app/components/Header.tsx
@@ -3,7 +3,11 @@ import React from "react"
 
 export default function Header() {
     return (
+<<<<<<< HEAD
         <header className="bg-slate-300 h-20 flex justify-center items-center
+=======
+        <header className="bg-neutral-100 h-20 flex justify-center items-center
+>>>>>>> parent of 3061960 (Changed background color and changed border radius)
             text-xl">
             <div className="flex gap-10">
                 <Link
--- dog-site-v3/src/app/page.tsx
+++ dog-site-v3/src/app/page.tsx
@@ -31,12 +31,20 @@ export default function Home() {
         minHeight: "100%", width: "100%", backgroundRepeat: "no-repeat",
         backgroundSize: "cover", backgroundPosition: "center", objectFit: "contain"
       }}>
+<<<<<<< HEAD
       <div className="bg-slate-300 px-4 py-2 flex flex-col gap-y-4">
+=======
+      <div className="bg-neutral-100 px-4 py-2 flex flex-col gap-y-4">
+>>>>>>> parent of 3061960 (Changed background color and changed border radius)
         <h1 className="text-center text-4xl">Dog Information Site</h1>
         <p className="text-center text-2xl">Find information about your favorite breed</p>
       </div>
       <Link
+<<<<<<< HEAD
         className="bg-slate-300 px-4 py-2 rounded text-xl hover:underline"
+=======
+        className="bg-neutral-100 px-4 py-2 rounded text-xl hover:underline"
+>>>>>>> parent of 3061960 (Changed background color and changed border radius)
         href="breeds"
       >
         Find dog information
