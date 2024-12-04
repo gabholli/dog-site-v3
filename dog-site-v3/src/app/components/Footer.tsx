e50d9b6cdc623b2ffe9267caa7580aae1b3902a7
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-12-04 11:49:29 -0800

    Revert "Merge pull request #4 from gabholli/feaeture/change-appearance"
    This reverts commit 989e4f79937b528189ebf081879a46b352e42362, reversing
    changes made to 7a374804c203de43e3e63198d623f15c64f6ef94.

--- dog-site-v3/src/app/components/Footer.tsx
+++ dog-site-v3/src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Footer() {
     return (
-        <footer className="bg-neutral-200 h-20 flex justify-center items-center
+        <footer className="bg-slate-300 h-20 flex justify-center items-center
             text-xl">
             <h1>&#169;2024 Dog Info Site</h1>
         </footer>
