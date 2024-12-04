7f60b34922cf8303c6b68bc1c8be87051d5c9b2a
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-12-04 11:41:17 -0800

    Changed background colors to whitesmoke

--- dog-site-v3/src/app/components/Header.tsx
+++ dog-site-v3/src/app/components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react"
 
 export default function Header() {
     return (
-        <header className="bg-neutral-200 h-20 flex justify-center items-center
+        <header className="bg-neutral-100 h-20 flex justify-center items-center
             text-xl">
             <div className="flex gap-10">
                 <Link
