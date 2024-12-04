81dd1ee237e26c8f68e0427e78f0362058b13a36
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-12-03 19:46:38 -0800

    Update page.tsx

--- dog-site-v3/src/app/page.tsx
+++ dog-site-v3/src/app/page.tsx
@@ -5,8 +5,10 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center gap-y-12 px-4
             bg-cover bg-center">
-      <h1 className="text-center text-4xl">Dog Information Site</h1>
-      <p className="text-center text-2xl">Find information about your favorite breed</p>
+      <div className="bg-slate-300 px-4 py-2 flex flex-col gap-y-4">
+        <h1 className="text-center text-4xl">Dog Information Site</h1>
+        <p className="text-center text-2xl">Find information about your favorite breed</p>
+      </div>
       <Link
         className="bg-slate-300 px-4 py-2 rounded text-xl hover:underline"
         href="breeds"
