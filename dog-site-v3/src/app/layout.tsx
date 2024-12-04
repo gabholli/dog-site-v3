5286256051f7147263dcc21aa1f00bd063a9e007
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-12-04 11:29:57 -0800

    Changed some background colors

--- dog-site-v3/src/app/layout.tsx
+++ dog-site-v3/src/app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from './components/Footer';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className='bg-clouds bg-center bg-cover grid grid-rows-[auto_1fr_auto] grid-cols-1 w-full min-h-screen'>
+      <body className='grid grid-rows-[auto_1fr_auto] grid-cols-1 w-full min-h-screen'>
         <Header />
         <main className="flex items-center justify-center">{children}</main>
         <Footer />
