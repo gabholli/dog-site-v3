8492d8489f80e12d2136ac658a0cff44d81747bc
Gabe Hollifield <gholl467@gmail.com>
2025-12-28 15:49:45 -0800

    Update Footer.tsx

--- dog-site-v3/src/app/components/Footer.tsx
+++ dog-site-v3/src/app/components/Footer.tsx
@@ -4,7 +4,7 @@ import RatingsStar from "./ratingsStar"
 export default function Footer() {
     return (
         <footer className="bg-neutral-100 h-20 flex justify-center items-center
-            gap-x-4 text-xl">
+            text-xl">
             <h1>&#169;2024 Dog Info Site</h1>
             <RatingsStar />
         </footer>
