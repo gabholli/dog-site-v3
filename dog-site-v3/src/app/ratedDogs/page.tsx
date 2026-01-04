1857180516347631a94f101e4af10e45384453be
Gabe Hollifield <gholl467@gmail.com>
2026-01-04 15:22:08 -0800

    Add "All" selection in select input field

--- dog-site-v3/src/app/ratedDogs/page.tsx
+++ dog-site-v3/src/app/ratedDogs/page.tsx
@@ -16,6 +16,7 @@ export default function RatedDogsList() {
     const [loading, setLoading] = useState<boolean>(false)
 
     const options = [
+        { value: null, label: "All" },
         { value: 1, label: "1" },
         { value: 2, label: "2" },
         { value: 3, label: "3" },
