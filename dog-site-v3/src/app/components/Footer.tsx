1f066ed58e6fceceb159c9c3238576570ec7b283
Gabe Hollifield <gholl467@gmail.com>
2026-05-03 20:07:45 -0700

    Update Footer.tsx

--- dog-site-v3/src/app/components/Footer.tsx
+++ dog-site-v3/src/app/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     return (
         <footer className="bg-neutral-100 h-20 flex justify-center items-center
             text-xl">
-            <h1>&#169;2025 Dog Info Site</h1>
+            <h1>&#169;2026 Dog Info Site</h1>
         </footer>
     )
 }
